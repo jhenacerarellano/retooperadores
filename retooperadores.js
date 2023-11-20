ef2490390567dff2e672d14b1f037594b128da92
jhenacerarellano <148493533+jhenacerarellano@users.noreply.github.com>
2023-11-20 10:52:37 -0600

    Add files via upload
    reto operadores

---
+++ retooperadores.js
@@ -0,0 +1,39 @@
+// variables.
+let numero1 = 8;
+let numero2 = 4;
+let texto1 ="texto1";
+let texto2 = "texto2";
+let objeto1 = {"edad": 26};
+let objeto2 = {"edad": 26};
+let valor1 = 10;
+let valor2 = 3;
+
+
+
+// constantes
+const escrito = "¿ el numero 1 es mayor que el numero 2? ";
+const escrito1 = "¿el numero 1 es menor que el numero 2? ";
+const escrito2 = "¿el numero 1 es igual al numero 2? ";
+const escrito3 = "¿el texto 1 es igual al texto 2? ";
+const escrito4 = "¿los objetos son iguales? ";
+const escrito5 = "AND Lógico: ";
+const escrito6 = "OR lÓGICO: ";
+const escrito7 = "la suma de 10 + 3: ";
+const escrito8 = "la resta de 10 - 3: ";
+const escrito9 = "la multiplicación de 10 * 3: ";
+const escrito10 = "la división de 10 * 3: ";
+const escrito11 = "10 modulo de 3: ";
+
+// operaciones
+console.log(escrito +(numero1>numero2));
+console.log(escrito1 + (numero1<numero2));
+console.log(escrito2 + (numero1 == numero2));
+console.log(escrito3 + (texto1 == texto2));
+console.log(escrito4 + (objeto1 == objeto2));
+console.log(escrito5 + (numero1<numero2));
+console.log(escrito6 + (numero1>numero2));
+console.log(escrito7 + (valor1 + valor2));
+console.log(escrito8 + (valor1 - valor2));
+console.log(escrito9 + (valor1 * valor2));
+console.log(escrito10 + (valor1 / valor2));
+console.log(escrito11 + (valor1 % valor2));
